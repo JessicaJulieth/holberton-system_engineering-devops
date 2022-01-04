69e25a3ee5fcf16915d5fef60569121ea95d5a94
JessicaJulieth <3497@holbertonschool.com>
2022-01-03 21:55:56 -0600

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 # Create a file
 file {'/tmp/school':                                                        # tipo de recurso y nombre de fichero
+        ensure  => present,
         path    => '/tmp/school',
         mode    => '0744',                                                      # permisos del fichero
         owner   => 'www-data'
