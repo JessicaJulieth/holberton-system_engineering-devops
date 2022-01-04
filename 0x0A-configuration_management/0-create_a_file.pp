2f2097e5c0d41f9000461706be5860db87dc2539
JessicaJulieth <3497@holbertonschool.com>
2022-01-03 22:30:14 -0600

    Create a file x

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { '/tmp/school':                                                        # tipo de recurso y nombre de fichero
   ensure  => 'file',
   mode    => '0744',                                                      # permisos del fichero
-  owner   => 'www-data'
-  group   => 'www-data'
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
