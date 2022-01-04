9b9d175ed0485dec5c416200964215953ad62eb3
JessicaJulieth <3497@holbertonschool.com>
2022-01-03 22:44:36 -0600

    Kill process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named killmenow
 
 exec { 'pkill killmenow':
-    command => "ps -ef | grep nrpe | grep -v pts/1 | /bin/awk '{print ${2}}' | xargs kill -9",
+    command => "pkill killmenow",
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
 }
