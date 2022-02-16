edfc0dde35b64e0569b1a5a64689047800a74f05
JessicaJulieth <3497@holbertonschool.com>
2022-02-15 22:49:22 -0500

    Requies the reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,12 @@ function that queries the Reddit API and returns the number of subscribers
 
 import requests
 
-def number_of_subscribers(subreddit)
 
+def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-            (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) \
+        Chrome/98.0.4758.82 Safari/537.36"}
     respon = requests.get(url, headers=headers, allow_redirects=False)
 
     if respon.status_code != 200:
